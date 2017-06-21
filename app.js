08461de259d271fe13024933feecad4a0a869eb0
ayanbabi90 <ayan.babi90@gmail.com>
2017-06-21 20:27:48 +0530

    Add app.js.

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//not generated varriables 
+
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var LocalStrategy = require('passport-local').Strategy;
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var flash = require('connect-flash');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,6 +42,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+
+//Handel Sessions
+app.use(session({
+	secret:'secret',
+	saveUninitilized: true,
+	resave: true
+}));
+
+// Passport For Auth
+app.use(passport.initialize());
+app.use(passport.session);
+
+// Validator
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+// express-message
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
